288c296e488473537def2979796a528e63832976
J. Pentz <jpentz1@gmail.com>
2024-01-09 23:22:39 -0700

    finished week 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,53 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(INIT)
+    @KEY
+    0;JMP
+
+(LOOP_INIT)
+    @i
+    M = 0
+(LOOP)
+    // 32 words per row, 256 rows
+    @i
+    D = M
+    @8192
+    D = A - D
+    @KEY
+    D;JLE
+
+    // Point to the screen addr + offset and then write 0
+    @SCREEN
+    D = A
+    @i
+    D = D + M
+    @R0
+    M = D
+    @color
+    D = M
+    @R0
+    A = M
+    M = D
+
+    // Increment by word size (16 bits)
+    @i
+    M = M + 1
+
+    @LOOP
+    0;JMP
+
+(KEY)
+    // Set screen to black if key is pressed
+    @color
+    M = -1
+    @KBD
+    D = M
+    @LOOP_INIT
+    D;JNE
+
+    // Otherwise Blank screen
+    @color
+    M = 0
+    @LOOP_INIT
+    0;JEQ
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,41 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+(INIT)
+    @R2
+    M = 0
+    @i
+    M = 0
+
+(LOOP)
+    // Check i
+    @R1
+    D = M
+    @i
+    D = D - M
+    @FOREVER
+    D; JEQ
+
+    // sum = sum + R0
+    @R0
+    D = M
+    @R2
+    M = D + M
+
+    // increment i
+    @i
+    M = M + 1
+    @LOOP
+    0;JMP
+
+(FOREVER)
+    @FOREVER
+    0;JMP
